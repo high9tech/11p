f23ba87b0d952d5f067b926b80d63d93bad68ebe
high9tech <hightechps96@gmail.com>
2026-08-08 20:50:38 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'hightech-ps-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './cache.manifest'
+];
+
+// تثبيت الكاش والتأكد من التجاوز السريع
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+// تفعيل وتحديث الكاش القديم
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// جلب الملفات من الكاش في حال عدم الاتصال بالإنترنت
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).then((networkResponse) => {
+        if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+          return networkResponse;
+        }
+        const responseToCache = networkResponse.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+        return networkResponse;
+      });
+    })
+  );
+});
